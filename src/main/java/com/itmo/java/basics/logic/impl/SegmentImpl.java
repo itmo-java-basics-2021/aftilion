57442f979a6916b1edbec28baae6ad0083a3e432
aftilion <aftis.sansan@gmail.com>
2021-03-28 17:24:21 +0300

    lab1
    ll

--- src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
@@ -122,7 +122,10 @@ public class SegmentImpl implements Segment {
 
     @Override
     public boolean delete(String objectKey) throws IOException {
-        
+
+        if (!segmentIndex.searchForKey(objectKey).isPresent()){
+            throw new IOException("Error while deleting");
+        }
         if (isReadOnly()) {
             outStream.close();
             return false;
