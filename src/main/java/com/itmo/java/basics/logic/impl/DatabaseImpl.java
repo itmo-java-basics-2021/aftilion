6038aaa79875161e5424f81b065832b7abfaae3b
Fredi Kats <fredikats@outlook.com>
2021-04-01 01:20:53 +0300

    init: add base code for second lab
    (cherry picked from commit 048b2e9026da74ec54d8b6fa1e2d7057384d3cb2)

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
@@ -2,6 +2,7 @@ package com.itmo.java.basics.logic.impl;
 
 import com.itmo.java.basics.exceptions.DatabaseException;
 import com.itmo.java.basics.index.impl.TableIndex;
+import com.itmo.java.basics.initialization.DatabaseInitializationContext;
 import com.itmo.java.basics.logic.Database;
 import com.itmo.java.basics.logic.Table;
 
@@ -45,6 +46,10 @@ public class DatabaseImpl implements Database {
      *                     поэтому при создании БД необходимо создать директорию внутри databaseRoot.
      */
     public static Database create(String dbName, Path databaseRoot) throws DatabaseException {
+        return null;
+    }
+
+    public static Database initializeFromContext(DatabaseInitializationContext context) {
         return null;
     }
 
