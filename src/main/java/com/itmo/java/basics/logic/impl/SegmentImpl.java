d73c491cfd6c8ec3f32d8b69eabb522e95a36f61
aftilion <aftis.sansan@gmail.com>
2021-04-02 16:12:28 +0300

    lab1

--- src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/SegmentImpl.java
@@ -52,25 +52,21 @@ public class SegmentImpl implements Segment {
     }
 
         Path segRoot = Paths.get(tableRootPath.toString(), segmentName);
-        //boolean fileExists;
-        //OutputStream outputStream;
+        boolean fileExists;
+        OutputStream outputStream;
 
-        try( OutputStream outputStream = Files.newOutputStream(segRoot) ) {
-            boolean fileExists;
+        try {
             fileExists = segRoot.toFile().createNewFile();
+            outputStream = Files.newOutputStream(segRoot);
 
-            if (!fileExists) {
-                throw new DatabaseException("Creating Error" + segmentName + "as it already exists");
-            }
-
-            return new SegmentImpl(segRoot, segmentName, outputStream);
         } catch (IOException ex) {
             throw new DatabaseException("Creating Error " + segmentName + ex);
         }
-        catch (DatabaseException ex){
-            throw new DatabaseException( "Creating error" + ex);
+        if (!fileExists) {
+            throw new DatabaseException("Creating Error" + segmentName + "as it already exists");
         }
-       // return new SegmentImpl(segRoot, segmentName, outputStream);
+
+        return new SegmentImpl(segRoot, segmentName, outputStream);
     }
 
     static String createSegmentName(String tableName) {
