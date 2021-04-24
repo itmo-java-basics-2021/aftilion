53a0bd6316428ce4776791d75b0339e449ef93ce
Aleksandor <aftis.sansan@gmail.com>
2021-04-24 04:14:01 +0300

    com

--- src/main/java/com/itmo/java/basics/logic/impl/CachingTable.java
+++ src/main/java/com/itmo/java/basics/logic/impl/CachingTable.java
@@ -11,24 +11,40 @@ import java.util.Optional;
  */
 public class CachingTable implements Table {
 
+    private Table myTable;
+    private DatabaseCache myDataBaseCache;
+
+    static final int MAX_CACHE_CAPACITY = 10000;
+
+    public CachingTable(Table myTable) {
+        this.myTable = myTable;
+        this.myDataBaseCache = new DatabaseCacheImpl(MAX_CACHE_CAPACITY);
+    }
+
     @Override
     public String getName() {
-      return null;
+        return myTable.getName();
     }
 
     @Override
     public void write(String objectKey, byte[] objectValue) throws DatabaseException {
-
-
+        myTable.write(objectKey, objectValue);
+        myDataBaseCache.set(objectKey, objectValue);
     }
 
     @Override
     public Optional<byte[]> read(String objectKey) throws DatabaseException {
-        return Optional.empty();
+        byte[] tryReadValueFromCache = myDataBaseCache.get(objectKey);
+        if (tryReadValueFromCache != null) {
+            return Optional.of(tryReadValueFromCache);
+        } else {
+            return myTable.read(objectKey);
+        }
     }
 
     @Override
     public void delete(String objectKey) throws DatabaseException {
-
+        myTable.delete(objectKey);
+        myDataBaseCache.delete(objectKey);
     }
 }
