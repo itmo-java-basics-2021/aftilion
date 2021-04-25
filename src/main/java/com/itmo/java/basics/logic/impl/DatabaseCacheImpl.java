a79e3bd900703b3d3ce6f8cb853b0a94703cf60c
Fredi Kats <fredikats@outlook.com>
2021-04-25 23:07:16 +0300

    feat: update docs

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseCacheImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseCacheImpl.java
@@ -2,36 +2,21 @@ package com.itmo.java.basics.logic.impl;
 
 import com.itmo.java.basics.logic.DatabaseCache;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class DatabaseCacheImpl implements DatabaseCache {
-
-    private static final int dbCapacity = 5000;
-    private final Map<String, byte[]> dbCache;
-
-    public DatabaseCacheImpl() {
-
-        this.dbCache = new LinkedHashMap<String, byte[]>(dbCapacity, 1f, true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<String, byte[]> eldest) {
-                return size() > dbCapacity;
-            }
-        };
-    }
+    private static final int CAPACITY = 5_000;
 
     @Override
     public byte[] get(String key) {
-        return dbCache.get(key);
+        return new byte[0];
     }
 
     @Override
     public void set(String key, byte[] value) {
-        dbCache.put(key, value);
+
     }
 
     @Override
     public void delete(String key) {
-        dbCache.remove(key);
+
     }
 }
