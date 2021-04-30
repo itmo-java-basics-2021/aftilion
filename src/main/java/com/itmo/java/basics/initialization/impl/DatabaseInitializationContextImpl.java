dfc38341cf0b6490fabfdeb71df933d0c653b3e9
Aleksandor <aftis.sansan@gmail.com>
2021-05-01 00:44:04 +0300

    1

--- src/main/java/com/itmo/java/basics/initialization/impl/DatabaseInitializationContextImpl.java
+++ src/main/java/com/itmo/java/basics/initialization/impl/DatabaseInitializationContextImpl.java
@@ -14,6 +14,8 @@ public class DatabaseInitializationContextImpl implements DatabaseInitialization
     private Path dbRoot;
     private Map<String, Table> tablesMap = new HashMap<>();
 
+    
+
     public DatabaseInitializationContextImpl(String dbName, Path databaseRoot) {
         this.dbName = dbName;
         this.dbRoot = Path.of(databaseRoot.toString(), dbName);
