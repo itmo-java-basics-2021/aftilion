396f20b02e9a84cb25895039f4294060d90e22a3
Aleksandor <aftis.sansan@gmail.com>
2021-04-30 23:10:43 +0300

    30/04

--- src/main/java/com/itmo/java/basics/config/DatabaseConfig.java
+++ src/main/java/com/itmo/java/basics/config/DatabaseConfig.java
@@ -5,9 +5,8 @@ import java.util.Objects;
 public class DatabaseConfig {
     public static final String DEFAULT_WORKING_PATH = "db_files";
     private final String workingPath;
-
     public DatabaseConfig(String workingPath) {
-        this.workingPath = workingPath;
+        this.workingPath = Objects.requireNonNullElse(workingPath, DEFAULT_WORKING_PATH);
     }
 
     public String getWorkingPath() {
