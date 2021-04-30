d923cb52d6b8faee806ce3f0fe082b93e2d43aca
Aleksandor <aftis.sansan@gmail.com>
2021-04-30 20:41:06 +0300

    30/04

--- src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/TableImpl.java
@@ -51,7 +51,7 @@ public class TableImpl implements Table {
     }
 
     public static Table initializeFromContext(TableInitializationContext context) {
-       return new TableImpl(context);
+        return new CachingTable(new TableImpl(context));
     }
 
     @Override
