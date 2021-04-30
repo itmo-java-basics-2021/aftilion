b3cf3b2fc9edb087c104ac317c81e5143024766d
Aleksandor <aftis.sansan@gmail.com>
2021-04-30 20:29:11 +0300

    30/04

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseCacheImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseCacheImpl.java
@@ -7,31 +7,30 @@ import java.util.Map;
 
 public class DatabaseCacheImpl implements DatabaseCache {
 
-    private final int capacity;
-    private final Map<String, byte[]> myCache;
+    private static final int dbCapacity = 5_000;
+    private final Map<String, byte[]> dbCache;
 
-    public DatabaseCacheImpl(int maxCapacity) {
-        this.capacity = maxCapacity;
-        this.myCache = new LinkedHashMap<String, byte[]>(capacity, 1f, true) {
+    public DatabaseCacheImpl() {
+        this.dbCache = new LinkedHashMap<String, byte[]>(dbCapacity, 1f, true) {
             @Override
             protected boolean removeEldestEntry(Map.Entry<String, byte[]> eldest) {
-                return size() > capacity;
+                return size() > dbCapacity;
             }
         };
     }
 
     @Override
     public byte[] get(String key) {
-        return myCache.get(key);
+        return dbCache.get(key);
     }
 
     @Override
     public void set(String key, byte[] value) {
-        myCache.put(key, value);
+        dbCache.put(key, value);
     }
 
     @Override
     public void delete(String key) {
-        myCache.remove(key);
+        dbCache.remove(key);
     }
 }
