e3aa3894df8449ff95249551da111b0980c275ba
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 00:54:29 +0300

    Create+Result in Console

--- src/main/java/com/itmo/java/basics/console/impl/CreateDatabaseCommand.java
+++ src/main/java/com/itmo/java/basics/console/impl/CreateDatabaseCommand.java
@@ -4,15 +4,22 @@ import com.itmo.java.basics.console.DatabaseCommand;
 import com.itmo.java.basics.console.DatabaseCommandArgPositions;
 import com.itmo.java.basics.console.DatabaseCommandResult;
 import com.itmo.java.basics.console.ExecutionEnvironment;
+import com.itmo.java.basics.exceptions.DatabaseException;
 import com.itmo.java.basics.logic.DatabaseFactory;
 import com.itmo.java.protocol.model.RespObject;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
  * Команда для создания базы данных
  */
 public class CreateDatabaseCommand implements DatabaseCommand {
+
+    private final ExecutionEnvironment environment;
+    private final DatabaseFactory dbfactory;
+    private final  List<RespObject> commandargs;
+    private static final int numberOfAgrguments = 3;
 
     /**
      * Создает команду.
@@ -21,12 +28,17 @@ public class CreateDatabaseCommand implements DatabaseCommand {
      *
      * @param env         env
      * @param factory     функция создания базы данных (пример: DatabaseImpl::create)
-     * @param commandArgs аргументы для создания (порядок - {@link DatabaseCommandArgPositions}.
+     * @param comArgs аргументы для создания (порядок - {@link DatabaseCommandArgPositions}.
      *                    Id команды, имя команды, имя создаваемой бд
      * @throws IllegalArgumentException если передано неправильное количество аргументов
      */
-    public CreateDatabaseCommand(ExecutionEnvironment env, DatabaseFactory factory, List<RespObject> commandArgs) {
-        //TODO implement
+    public CreateDatabaseCommand(ExecutionEnvironment env, DatabaseFactory factory, List<RespObject> comArgs) {
+        if (comArgs.size() != numberOfAgrguments){
+            throw new IllegalArgumentException("Why " + comArgs.size()+"!= 3 ,  in CreateDataBaseCommand" );
+        }
+        environment = env;
+        dbfactory = factory;
+        commandargs = comArgs;
     }
 
     /**
@@ -36,7 +48,15 @@ public class CreateDatabaseCommand implements DatabaseCommand {
      */
     @Override
     public DatabaseCommandResult execute() {
-        //TODO implement
-        return null;
+       try{
+           String dbName = commandargs.get(DatabaseCommandArgPositions.DATABASE_NAME.getPositionIndex()).asString();
+           if(dbName == null){
+               throw new DatabaseException("Why dbname null? ");
+           }
+           environment.addDatabase(dbfactory.createNonExistent(dbName,environment.getWorkingPath()));
+           return DatabaseCommandResult.success(("Success add " + dbName).getBytes(StandardCharsets.UTF_8));
+       } catch (DatabaseException ex){
+           return new FailedDatabaseCommandResult(ex.getMessage());
+       }
     }
 }
