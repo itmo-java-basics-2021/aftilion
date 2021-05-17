eed7ba2b5297debf91e535091b5315570fb87e5b
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 04:55:47 +0300

    3lab

--- src/main/java/com/itmo/java/basics/logic/impl/CachingTable.java
+++ src/main/java/com/itmo/java/basics/logic/impl/CachingTable.java
@@ -6,9 +6,6 @@ import com.itmo.java.basics.logic.Table;
 
 import java.util.Optional;
 
-/**
- * Декоратор для таблицы. Кэширует данные
- */
 public class CachingTable implements Table {
 
     private final Table cacheTable;
