aa7e763725eea1f12bd31600aefaefbc49733bbd
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 03:28:28 +0300

    server

--- src/main/java/com/itmo/java/client/connection/DirectReferenceKvsConnection.java
+++ src/main/java/com/itmo/java/client/connection/DirectReferenceKvsConnection.java
@@ -5,20 +5,27 @@ import com.itmo.java.client.exception.ConnectionException;
 import com.itmo.java.protocol.model.RespArray;
 import com.itmo.java.protocol.model.RespObject;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * Реализация подключения, когда есть прямая ссылка на объект
  * (пока еще нет реализации сокетов)
  */
 public class DirectReferenceKvsConnection implements KvsConnection {
 
+    private final DatabaseServer dbServer;
+
     public DirectReferenceKvsConnection(DatabaseServer databaseServer) {
-        //TODO implement
+        dbServer = databaseServer;
     }
 
     @Override
     public RespObject send(int commandId, RespArray command) throws ConnectionException {
-        //TODO implement
-        return null;
+        try{
+            return dbServer.executeNextCommand(command).get().serialize();
+        } catch(ExecutionException | InterruptedException ex){
+            throw new ConnectionException(ex.getMessage(), ex.getCause());
+        }
     }
 
     /**
