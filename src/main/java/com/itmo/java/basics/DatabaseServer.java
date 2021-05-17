eab8402c868578864b6eb56eb0a038db7fbaa617
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 04:20:20 +0300

    ser

--- src/main/java/com/itmo/java/basics/DatabaseServer.java
+++ src/main/java/com/itmo/java/basics/DatabaseServer.java
@@ -2,8 +2,7 @@ package com.itmo.java.basics;
 
 import com.itmo.java.basics.console.*;
 import com.itmo.java.basics.exceptions.DatabaseException;
-import com.itmo.java.basics.initialization.impl.DatabaseServerInitializer;
-import com.itmo.java.basics.initialization.impl.InitializationContextImpl;
+import com.itmo.java.basics.initialization.impl.*;
 import com.itmo.java.protocol.model.RespArray;
 
 import java.util.concurrent.CompletableFuture;
@@ -12,28 +11,30 @@ import java.util.concurrent.Executors;
 
 public class DatabaseServer {
 
-    private ExecutorService executorService = Executors.newSingleThreadExecutor();
-    private final ExecutionEnvironment environment;
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final ExecutionEnvironment enviroment;
+
     /**
-     * Конструктор
+     * Con structor
      *
-     * @param env         env для инициализации. Далее работа происходит с заполненным объектом
+     * @param env         env для инициализации. Далее работа происходит с заполненым объектом
      * @param initializer готовый чейн инициализации
      * @throws DatabaseException если произошла ошибка инициализации
      */
-    private DatabaseServer(ExecutionEnvironment env){
-        environment = env;
-    }
     public static DatabaseServer initialize(ExecutionEnvironment env, DatabaseServerInitializer initializer) throws DatabaseException {
-        initializer.perform(new InitializationContextImpl(env,null,null,null));
+        initializer.perform(new InitializationContextImpl(env, null, null, null));
         return new DatabaseServer(env);
     }
 
+    private DatabaseServer(ExecutionEnvironment env) {
+        this.enviroment = env;
+    }
+
     public CompletableFuture<DatabaseCommandResult> executeNextCommand(RespArray message) {
-        return CompletableFuture.supplyAsync(() -> DatabaseCommands.valueOf(message.getObjects().get(DatabaseCommandArgPositions.COMMAND_NAME.getPositionIndex()).asString()).getCommand(environment, message.getObjects()).execute(), executorService);
+        return CompletableFuture.supplyAsync(() -> DatabaseCommands.valueOf(message.getObjects().get(DatabaseCommandArgPositions.COMMAND_NAME.getPositionIndex()).asString()).getCommand(enviroment, message.getObjects()).execute(), executorService);
     }
 
     public CompletableFuture<DatabaseCommandResult> executeNextCommand(DatabaseCommand command) {
-        return CompletableFuture.supplyAsync(command::execute,executorService);
+        return CompletableFuture.supplyAsync(command::execute, executorService);
     }
 }
