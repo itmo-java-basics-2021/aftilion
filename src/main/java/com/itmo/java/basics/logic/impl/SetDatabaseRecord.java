f82e601d0bcf73454418c1d155a17e36ba88e8f7
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 04:32:53 +0300

    1

--- src/main/java/com/itmo/java/basics/logic/impl/SetDatabaseRecord.java
+++ src/main/java/com/itmo/java/basics/logic/impl/SetDatabaseRecord.java
@@ -6,13 +6,15 @@ import com.itmo.java.basics.logic.WritableDatabaseRecord;
  * Запись в БД, означающая добавление значения по ключу
  */
 public class SetDatabaseRecord implements WritableDatabaseRecord {
-    private final byte[] key;
-    private final byte[] value;
-    public SetDatabaseRecord(byte[] key, byte[] value)
-    {
+
+    private byte[] key;
+    private byte[] value;
+
+    public SetDatabaseRecord(byte[] key, byte[] value) {
         this.key = key;
         this.value = value;
     }
+
 
     @Override
     public byte[] getKey() {
@@ -24,10 +26,6 @@ public class SetDatabaseRecord implements WritableDatabaseRecord {
         return value;
     }
 
-    /**
-     *
-     * @return размер записи, где 8 - размер записи чисел, равных размерам ключа и значения( 2 размера Integer в байтах)
-     */
     @Override
     public long size() {
         return getKeySize() + getValueSize() + 4 + 4;
