73b3724ecbcf66316fcbcc59489019f688335a94
Aleksandor <aftis.sansan@gmail.com>
2021-05-17 04:34:15 +0300

    Update SegmentInitializationContextImpl.java

--- src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializationContextImpl.java
+++ src/main/java/com/itmo/java/basics/initialization/impl/SegmentInitializationContextImpl.java
@@ -7,43 +7,47 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SegmentInitializationContextImpl implements SegmentInitializationContext {
+    private final String name;
+    private final Path path;
+    private final SegmentIndex index;
+    private final long size;
 
-    private final String segmentName;
-    private final Path segmentPath;
-    private final long currentSize;
-    private final SegmentIndex segmentIndex;
-
-    public SegmentInitializationContextImpl(String segmentName, Path segmentPath, int currentSize, SegmentIndex index) {
-        this.segmentName = segmentName;
-        this.segmentPath = segmentPath;
-        this.currentSize = currentSize;
-        this.segmentIndex = index;
+    public SegmentInitializationContextImpl(String segmentName, Path segmentPath, long currentSize, SegmentIndex index) {
+        name = segmentName;
+        path = segmentPath;
+        size = currentSize;
+        this.index = index;
     }
 
-    public SegmentInitializationContextImpl(String segmentName, Path tablePath, int currentSize) {
-        this.segmentName = segmentName;
-        this.segmentPath = Paths.get(tablePath.toString(), segmentName);
-        this.currentSize = currentSize;
-        this.segmentIndex = new SegmentIndex();
+
+    /**
+     * Не используйте этот конструктор. Оставлен для совместимости со старыми тестами.
+     */
+    public SegmentInitializationContextImpl(String segmentName, Path tablePath, long currentSize) {
+        this(segmentName, tablePath.resolve(segmentName), currentSize, new SegmentIndex());
+    }
+
+    public SegmentInitializationContextImpl(String segmentName, Path tablePath) {
+        this(segmentName, tablePath.resolve(segmentName), 0, new SegmentIndex());
     }
 
     @Override
     public String getSegmentName() {
-        return segmentName;
+        return name;
     }
 
     @Override
     public Path getSegmentPath() {
-        return segmentPath;
+        return path;
     }
 
     @Override
     public SegmentIndex getIndex() {
-        return segmentIndex;
+        return index;
     }
 
     @Override
     public long getCurrentSize() {
-        return currentSize;
+        return size;
     }
 }
