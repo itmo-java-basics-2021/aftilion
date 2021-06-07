3a3343f9e10033c3c7e95e4f5e89d887b56add86
Aleksandor <aftis.sansan@gmail.com>
2021-06-08 02:50:13 +0300

    firstSocketKvsConnection

--- src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
+++ src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
@@ -4,27 +4,51 @@ import com.itmo.java.client.exception.ConnectionException;
 import com.itmo.java.protocol.RespReader;
 import com.itmo.java.protocol.RespWriter;
 import com.itmo.java.protocol.model.RespArray;
+import com.itmo.java.protocol.model.RespCommandId;
 import com.itmo.java.protocol.model.RespObject;
+
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * С помощью {@link RespWriter} и {@link RespReader} читает/пишет в сокет
  */
 public class SocketKvsConnection implements KvsConnection {
 
+    private Socket socket;
+    private RespReader respReader;
+    private RespWriter respWriter;
+
     public SocketKvsConnection(ConnectionConfig config) {
-        //TODO implement
+          try {
+              socket = new Socket(config.toString(), config.getPort());
+              respReader = new RespReader(socket.getInputStream());
+              respWriter = new RespWriter(socket.getOutputStream());
+          } catch (IOException ex) {
+              ex.toString();
+          }
     }
 
     /**
      * Отправляет с помощью сокета команду и получает результат.
+     *
      * @param commandId id команды (номер)
      * @param command   команда
      * @throws ConnectionException если сокет закрыт или если произошла другая ошибка соединения
      */
     @Override
     public synchronized RespObject send(int commandId, RespArray command) throws ConnectionException {
-        //TODO implement
-        return null;
+        try {
+
+            respWriter.write(new RespArray(new RespCommandId(commandId),command));
+            RespObject respObject = respReader.readObject();
+            if (respObject.isError()) {
+                throw new ConnectionException("Connection error respObgect is error");
+            }
+            return  respObject;
+        } catch (IOException ex) {
+            throw new ConnectionException("Error while sendinh in SocketKvsConnection" , ex);
+        }
     }
 
     /**
@@ -32,6 +56,13 @@ public class SocketKvsConnection implements KvsConnection {
      */
     @Override
     public void close() {
-        //TODO implement
+        try {
+            socket.close();
+            respReader.close();
+            respWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
