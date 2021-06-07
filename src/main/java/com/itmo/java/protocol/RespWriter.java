a8efb409091d289e7c683eac4b9b542a1e1fba43
Aleksandor <aftis.sansan@gmail.com>
2021-06-08 01:14:55 +0300

    addWriter

--- src/main/java/com/itmo/java/protocol/RespWriter.java
+++ src/main/java/com/itmo/java/protocol/RespWriter.java
@@ -4,41 +4,24 @@ import com.itmo.java.protocol.model.RespObject;
 
 import java.io.IOException;
 import java.io.OutputStream;
+
 public class RespWriter implements AutoCloseable{
 
+    private final OutputStream outputStream;
+
     public RespWriter(OutputStream os) {
-        //TODO implement
+       outputStream = os;
     }
 
     /**
      * Записывает в output stream объект
      */
     public void write(RespObject object) throws IOException {
-        //TODO implement
+        object.write(outputStream);
     }
 
     @Override
     public void close() throws IOException {
-        //TODO implement
+        outputStream.close();
     }
 }
-//public class RespWriter implements AutoCloseable{
-//
-//    private final OutputStream outputStream;
-//
-//    public RespWriter(OutputStream os) {
-//       outputStream = os;
-//    }
-//
-//    /**
-//     * Записывает в output stream объект
-//     */
-//    public void write(RespObject object) throws IOException {
-//        object.write(outputStream);
-//    }
-//
-//    @Override
-//    public void close() throws IOException {
-//        outputStream.close();
-//    }
-//}
