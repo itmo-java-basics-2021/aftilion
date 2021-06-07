e287ac6fcc173f38b61092dd0c8abafced26600a
Aleksandor <aftis.sansan@gmail.com>
2021-06-08 02:27:19 +0300

    CommandReader

--- src/main/java/com/itmo/java/basics/resp/CommandReader.java
+++ src/main/java/com/itmo/java/basics/resp/CommandReader.java
@@ -1,23 +1,29 @@
 package com.itmo.java.basics.resp;
 
 import com.itmo.java.basics.console.DatabaseCommand;
+import com.itmo.java.basics.console.DatabaseCommandArgPositions;
+import com.itmo.java.basics.console.DatabaseCommands;
 import com.itmo.java.basics.console.ExecutionEnvironment;
 import com.itmo.java.protocol.RespReader;
+import com.itmo.java.protocol.model.RespObject;
 
 import java.io.IOException;
+import java.util.List;
 
 public class CommandReader implements AutoCloseable {
 
+    private final RespReader respReader;
+    private final ExecutionEnvironment executionEnvironment;
     public CommandReader(RespReader reader, ExecutionEnvironment env) {
-        //TODO implement
+        respReader = reader;
+        executionEnvironment = env;
     }
 
     /**
      * Есть ли следующая команда в ридере?
      */
     public boolean hasNextCommand() throws IOException {
-        //TODO implement
-        return false;
+       return respReader.hasArray();
     }
 
     /**
@@ -26,12 +32,16 @@ public class CommandReader implements AutoCloseable {
      * @throws IllegalArgumentException если нет имени команды и id
      */
     public DatabaseCommand readCommand() throws IOException {
-        //TODO implement
-        return null;
+        List<RespObject> comArgs = respReader.readArray().getObjects();
+        if(comArgs.size() < 2) {
+            throw new IllegalArgumentException("Why we dont have id and command name?");
+        }
+        return DatabaseCommands.valueOf(comArgs.get(DatabaseCommandArgPositions.COMMAND_NAME.getPositionIndex()).asString())
+                .getCommand(executionEnvironment,comArgs);
     }
 
     @Override
     public void close() throws Exception {
-        //TODO implement
+       respReader.close();
     }
 }
