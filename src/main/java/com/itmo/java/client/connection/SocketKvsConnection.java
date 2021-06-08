30b26493377d21e7874b94d9fcaca1d0d24b71a6
Aleksandor <aftis.sansan@gmail.com>
2021-06-09 01:11:03 +0300

    try socket

--- src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
+++ src/main/java/com/itmo/java/client/connection/SocketKvsConnection.java
@@ -4,6 +4,7 @@ import com.itmo.java.client.exception.ConnectionException;
 import com.itmo.java.protocol.RespReader;
 import com.itmo.java.protocol.RespWriter;
 import com.itmo.java.protocol.model.RespArray;
+import com.itmo.java.protocol.model.RespCommandId;
 import com.itmo.java.protocol.model.RespObject;
 
 import java.io.IOException;
@@ -13,21 +14,24 @@ import java.net.Socket;
  * С помощью {@link RespWriter} и {@link RespReader} читает/пишет в сокет
  */
 public class SocketKvsConnection implements KvsConnection {
-    final ConnectionConfig config;
-    final Socket socket;
+
+    private Socket socket;
+    private RespWriter respWriter;
+    private RespReader respReader;
 
     public SocketKvsConnection(ConnectionConfig config) {
-        this.config = config;
-
         try {
-            this.socket = new Socket(this.config.getHost(), this.config.getPort());
-        } catch (IOException exception) {
-            throw new RuntimeException("Creation socket error", exception);
+            socket = new Socket(config.getHost(), config.getPort());
+            respReader = new RespReader(socket.getInputStream());
+            respWriter = new RespWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     /**
      * Отправляет с помощью сокета команду и получает результат.
+     *
      * @param commandId id команды (номер)
      * @param command   команда
      * @throws ConnectionException если сокет закрыт или если произошла другая ошибка соединения
@@ -35,20 +39,10 @@ public class SocketKvsConnection implements KvsConnection {
     @Override
     public synchronized RespObject send(int commandId, RespArray command) throws ConnectionException {
         try {
-            final RespWriter respWriter = new RespWriter(socket.getOutputStream());
-
-            respWriter.write(command);
-
-            final RespReader respReader = new RespReader(socket.getInputStream());
-            final RespObject respObject = respReader.readObject();
-
-            if (respObject.isError()) {
-                throw new ConnectionException("Response error");
-            }
-
-            return respObject;
-        } catch (IOException exception) {
-            throw new ConnectionException("Connection exception", exception);
+            respWriter.write(new RespArray(new RespCommandId(commandId) ,command));
+            return respReader.readObject();
+        } catch (IOException e) {
+            throw new ConnectionException("?", e);
         }
     }
 
@@ -59,8 +53,10 @@ public class SocketKvsConnection implements KvsConnection {
     public void close() {
         try {
             socket.close();
+            respWriter.close();
+            respReader.close();
         } catch (IOException exception) {
-            throw new RuntimeException("Closing client socket error", exception);
+            exception.printStackTrace();
         }
     }
 }
