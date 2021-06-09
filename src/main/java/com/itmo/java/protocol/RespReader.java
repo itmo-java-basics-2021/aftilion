705fedc57b8b81b2adc524e2b72b492a26d7951b
Aleksandor <aftis.sansan@gmail.com>
2021-06-09 18:19:29 +0300

    trying without

--- src/main/java/com/itmo/java/protocol/RespReader.java
+++ src/main/java/com/itmo/java/protocol/RespReader.java
@@ -133,7 +133,7 @@ public class RespReader implements AutoCloseable {
                 throw new EOFException("Stream is empty when try to read all bytes before '\\r\\n'");
             }
             if (currentByte == CR) {
-                reader.mark(READ_AHEAD_LIMIT);
+              //  reader.mark(READ_AHEAD_LIMIT);
                 int nextByte = reader.read();
                 if (nextByte == -1) {
                     throw new EOFException("Stream is empty when try to read all bytes before '\\r\\n'");
