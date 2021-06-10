d36144742a66aa9dbe2e7d8d4eb7ed8eded6efab
Aleksandor <aftis.sansan@gmail.com>
2021-06-10 16:41:14 +0300

    откат del

--- src/main/java/com/itmo/java/basics/console/impl/DeleteKeyCommand.java
+++ src/main/java/com/itmo/java/basics/console/impl/DeleteKeyCommand.java
@@ -8,8 +8,6 @@ import com.itmo.java.basics.exceptions.DatabaseException;
 import com.itmo.java.basics.logic.Database;
 import com.itmo.java.protocol.model.RespObject;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,28 +15,26 @@ import java.util.Optional;
  * Команда для создания удаления значения по ключу
  */
 public class DeleteKeyCommand implements DatabaseCommand {
-
-    private final ExecutionEnvironment environment;
-    private final List<RespObject> commandargs;
-    private static final int numberOfAgrguments = 5;
+    private final ExecutionEnvironment env;
+    private final String databaseName;
+    private final String tableName;
+    private final String key;
 
     /**
      * Создает команду.
      * <br/>
      * Обратите внимание, что в конструкторе нет логики проверки валидности данных. Не проверяется, можно ли исполнить команду. Только формальные признаки (например, количество переданных значений или ненуловость объектов
      *
-     * @param env     env
-     * @param comArgs аргументы для создания (порядок - {@link DatabaseCommandArgPositions}.
-     *                Id команды, имя команды, имя бд, таблицы, ключ
+     * @param env         env
+     * @param commandArgs аргументы для создания (порядок - {@link DatabaseCommandArgPositions}.
+     *                    Id команды, имя команды, имя бд, таблицы, ключ
      * @throws IllegalArgumentException если передано неправильное количество аргументов
      */
-    public DeleteKeyCommand(ExecutionEnvironment env, List<RespObject> comArgs)  {
-
-        if (comArgs.size() != numberOfAgrguments) {
-            throw new IllegalArgumentException("Why " + comArgs.size() + "!= 5 , in CreateTableCommand");
-        }
-        environment = env;
-        commandargs = comArgs;
+    public DeleteKeyCommand(ExecutionEnvironment env, List<RespObject> commandArgs) {
+        this.env = env;
+        this.databaseName = commandArgs.get(DatabaseCommandArgPositions.DATABASE_NAME.getPositionIndex()).asString();
+        this.tableName = commandArgs.get(DatabaseCommandArgPositions.TABLE_NAME.getPositionIndex()).asString();
+        this.key = commandArgs.get(DatabaseCommandArgPositions.KEY.getPositionIndex()).asString();
     }
 
     /**
@@ -49,30 +45,18 @@ public class DeleteKeyCommand implements DatabaseCommand {
     @Override
     public DatabaseCommandResult execute() {
         try {
-            String dbName = commandargs.get(DatabaseCommandArgPositions.DATABASE_NAME.getPositionIndex()).asString();
-            if (dbName == null) {
-                throw new DatabaseException("Why dbname is null?");
+            Optional<Database> database = env.getDatabase(databaseName);
+            if (database.isEmpty()){
+                return DatabaseCommandResult.error("Not found database " + databaseName);
             }
-            String tbName = commandargs.get(DatabaseCommandArgPositions.TABLE_NAME.getPositionIndex()).asString();
-            if (tbName == null) {
-                throw new DatabaseException("Why tbName is null?");
+            Optional<byte[]> value = database.get().read(tableName, key);
+            if (value.isEmpty()){
+                return DatabaseCommandResult.error("Value with key " + key + " in database " + databaseName + " not found");
             }
-            String key = commandargs.get(DatabaseCommandArgPositions.KEY.getPositionIndex()).asString();
-            if (key == null) {
-                throw new DatabaseException("Why key is null?");
-            }
-            Optional<Database> dataBase = environment.getDatabase(dbName);
-            if (dataBase.isEmpty()) {
-                throw new DatabaseException("We dont have" + dbName);
-            }
-            Optional<byte[]> previous = dataBase.get().read(tbName,key);
-            if (previous.isEmpty()) {
-                return DatabaseCommandResult.error("Error deleteKeyCommand");
-            }
-            dataBase.get().delete(tbName, key);
-            return DatabaseCommandResult.success(("Success del " + dbName + tbName + key).getBytes(StandardCharsets.UTF_8));
-        } catch (DatabaseException ex) {
-            return new FailedDatabaseCommandResult(ex.getMessage());
+            database.get().delete(tableName, key);
+            return DatabaseCommandResult.success(value.get());
+        } catch (DatabaseException e){
+            return DatabaseCommandResult.error("DatabaseException when try to delete value by key " + key + " in table " + tableName);
         }
     }
 }
