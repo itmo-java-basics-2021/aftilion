164ddb3db7f66a0ca5b6dd19d6d4db9367d67bda
Aleksandor <aftis.sansan@gmail.com>
2021-06-10 03:05:39 +0300

    - gradle

--- src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
+++ src/main/java/com/itmo/java/basics/connector/JavaSocketServerConnector.java
@@ -77,6 +77,11 @@ public class JavaSocketServerConnector implements Closeable {
 
 
     public static void main(String[] args) throws Exception {
+        DatabaseServerConfig config = new ConfigLoader().readConfig();
+        DatabaseServer server = DatabaseServer.initialize(new ExecutionEnvironmentImpl(config.getDbConfig()),
+                new DatabaseServerInitializer(new DatabaseInitializer(new TableInitializer(new SegmentInitializer()))));
+        JavaSocketServerConnector connector = new JavaSocketServerConnector(server, config.getServerConfig());
+        connector.start();
     }
 
     /**
