dc903728eadd51a10a0ed895507d9864d25edbcb
Aleksandor <aftis.sansan@gmail.com>
2021-06-10 20:59:44 +0300

    try2

--- src/main/java/com/itmo/java/basics/console/impl/ExecutionEnvironmentImpl.java
+++ src/main/java/com/itmo/java/basics/console/impl/ExecutionEnvironmentImpl.java
@@ -5,18 +5,19 @@ import com.itmo.java.basics.console.ExecutionEnvironment;
 import com.itmo.java.basics.logic.Database;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class ExecutionEnvironmentImpl implements ExecutionEnvironment {
 
-    private final DatabaseConfig dbConfig;
-    private final Map<String, Database> dataBase;
+
+    Map<String, Database> dataBase;
+    Path workingPath;
 
     public ExecutionEnvironmentImpl(DatabaseConfig config) {
-        dbConfig = config;
-        dataBase = new HashMap<>();
+        workingPath = Paths.get(config.getWorkingPath());
     }
 
     @Override
@@ -31,6 +32,6 @@ public class ExecutionEnvironmentImpl implements ExecutionEnvironment {
 
     @Override
     public Path getWorkingPath() {
-        return Path.of(dbConfig.getWorkingPath());
+        return workingPath;
     }
 }
